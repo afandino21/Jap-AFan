5b615b923a9ef9ba48a5994252c366c130e39d13
¨afandino21¨ <al_fl95@hotmail.com>
2023-11-29 12:06:08 -0300

    some changes

---
+++ 14.1-Bootstrap-y-Responsive/js/index.js
@@ -0,0 +1,5 @@
+function toggleOptions() {
+    const divOpciones = document.getElementById("divOptionsNav");
+    divOpciones.classList.toggle('d-none');
+    divOpciones.classList.toggle('d-block');
+};
